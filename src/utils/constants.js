0e865a318e0ebc631746eea05b8d2d12c9b3f802
Altowashere <your.email@example.com>
2026-03-07 13:25:07 -0600

    part of constants done

---
+++ src/utils/constants.js
@@ -0,0 +1,25 @@
+import { LiaBell } from "react-icons/lia";
+
+export const Personal_info = {
+  name: "A",
+  title: "Programmer",
+  email: "CoolTempEmail@gmail.com",
+  location: "Virtual",
+  tagline: "Cool tuff line",
+  resume: "public\temp resume file annoation reading.pdf",
+  bio: ["Hey Tuff temp bio", "Cool temp bio again", "Another temp bio"],
+};
+
+export const Social_links = {
+  github: "https://github.com/",
+  twitter: "https://x.com/",
+  discord: "https://discord.com/",
+  gmail: "https://gmail.com/",
+};
+
+export const STATS = [
+  { label: "Years Of Experience", value: "2+" },
+  { label: "Projects Completed", value: "5+" },
+  { label: "Programming Languages", value: "3+" },
+  { label: "Client satisfaction", value: "98%" },
+];
